2ee283c210f9a2bb1721cc9a31a06292966d74df
sebastianfaber <sebastian@startup-werk.de>
2024-02-16 12:11:28 +0100

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+# Install Docker GitHub Action
+
+This GitHub Action installs Docker on the runner machine.
+
+## Usage
+
+To use this action, include it in your workflow file:
+
+```yaml
+steps:
+- name: Initialize Docker
+  uses: fase22/initdockeraction
+
---
+++ index.js
@@ -0,0 +1,41 @@
+const { exec } = require('child_process');
+const core = require('@actions/core');
+
+async function run() {
+  try {
+    // Install Docker
+    core.info('Installing Docker...');
+    await execCommand(`
+      export DEBIAN_FRONTEND=noninteractive
+      apt-get update
+      apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
+      curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+      echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+      apt-get update
+      apt-get install -y docker-ce docker-ce-cli containerd.io
+    `);
+    core.info('Docker installed successfully.');
+
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+function execCommand(command) {
+  return new Promise((resolve, reject) => {
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        reject(error);
+        return;
+      }
+      if (stderr) {
+        reject(stderr);
+        return;
+      }
+      resolve(stdout);
+    });
+  });
+}
+
+run();
+
